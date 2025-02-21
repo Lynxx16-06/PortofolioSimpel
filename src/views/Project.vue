c7ac12414793838ddb63027df142eff4f7a5510a
Nama Anda <email@anda.com>
2025-02-21 12:34:43 +0700

    update

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -16,7 +16,7 @@
             </div>
           </div>
           <div>
-            <label for="last-name" class="block text-sm/6 font-semibold text-gray-900">Last name</label>
+            <label for="last-name" class="block text-sm/6 font-semibold text-white">Last name</label>
             <div class="mt-2.5">
               <input type="text" name="last-name" id="last-name" placeholder="Nama Belakang" autocomplete="family-name" class="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
             </div>
